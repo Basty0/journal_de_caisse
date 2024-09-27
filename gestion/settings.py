02302273d2e335ff36ec867efca526edb1f51111
Basty0 <126388152+Basty0@users.noreply.github.com>
2024-09-27 19:29:14 +0300

    Intégration de la librairie tailwind css et configuration

--- gestion/settings.py
+++ gestion/settings.py
@@ -16,7 +16,11 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+TAILWIND_APP_NAME = 'theme'
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
+NPM_BIN_PATH = r"C:\Program Files\nodejs\npm.cmd"
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
@@ -39,6 +43,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'caisse',
+     'tailwind',
+     'theme',
+    
 ]
 
 MIDDLEWARE = [
